8c2beb191d04fea93391e2c1fbeaab98674efc71
SumterUtilitiesInc <shtaylor@suimail.com>
2022-05-29 09:35:41 -0400

    Add common utilities

---
+++ SandlotWizards_dotnet_core/src/SandlotWizards/Services/Common/Common/DateTimeUtilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SandlotWizards.Common
+{
+    public static class DateTimeUtilities
+    {
+
+        public static string GetCurrentDate()
+        {
+            return GeneralUtilities.addZeros(DateTime.Now.Year.ToString(), 4, true) + "-" +
+                   GeneralUtilities.addZeros(DateTime.Now.Month.ToString().Trim(), 2, true) +
+                   GeneralUtilities.addZeros(DateTime.Now.Day.ToString().Trim(), 2, true);
+        }
+
+        public static string ConvertYYYYMMDDtoMMDDYY(string YYYYMMDD)
+        {
+            return YYYYMMDD.Substring(5, 2) + "/" + YYYYMMDD.Substring(8, 2) + "/" + YYYYMMDD.Substring(2, 2);
+        }
+        public static string ConvertDateTimetoYYYYMMDD(DateTime dt)
+        {
+            return GeneralUtilities.addZeros(dt.Year.ToString(), 4, true) + "-" + GeneralUtilities.addZeros(dt.Month.ToString(), 2, true) + "-" + GeneralUtilities.addZeros(dt.Day.ToString(), 2, true);
+        }
+
+        public static string ConvertMMDDYYYY_TimeToYYYYMMDD(string MMDDYYYY_Time)
+        {
+            if (MMDDYYYY_Time.Trim().Length == 0) return "0001-01-01";
+
+            string[] dateArray = MMDDYYYY_Time.Split(' ')[0].Split('/');
+            return GeneralUtilities.addZeros(dateArray[2], 4, true) + "-" + GeneralUtilities.addZeros(dateArray[0], 2, true) + "-" + GeneralUtilities.addZeros(dateArray[1], 2, true);
+        }
+
+        public static string ConvertMMDDYYYYToYYYYMMDD(string MMDDYYYY)
+        {
+            string[] dateArray = MMDDYYYY.Split('/');
+            return GeneralUtilities.addZeros(dateArray[2], 4, true) + "-" + GeneralUtilities.addZeros(dateArray[0], 2, true) + "-" + GeneralUtilities.addZeros(dateArray[1], 2, true);
+        }
+        public static string stripTimeFromDateString(string source)
+        {
+            return source.Split(' ')[0];
+        }
+    }
+}
---
+++ SandlotWizards_dotnet_core/src/SandlotWizards/Services/Common/Common/GeneralUtilities.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+
+namespace SandlotWizards.Common
+{
+    public static class GeneralUtilities
+    {
+        public static DataColumn CreateDataColumn(string columnName, Type datatype, string columnHeader)
+        {
+            DataColumn col = new DataColumn(columnName, datatype);
+            col.Caption = columnHeader;
+            return col;
+        }
+
+        public static string ConvertDataSetToCSV(DataSet dataset, bool useDoubleQuotesAroundColumnValue)
+        {
+            string csvString = "";
+
+            string headerString = "";
+            foreach (DataColumn dataColumn in dataset.Tables[0].Columns)
+            {
+                if (headerString.Length > 0) { headerString += ","; }
+                string columnName = dataColumn.Caption;
+                headerString += $"\"{columnName}\"";
+            }
+            headerString += "\r\n";
+            csvString += headerString;
+
+            foreach (DataRow dataRow in dataset.Tables[0].Rows)
+            {
+                string lineString = "";
+                foreach (DataColumn dataColumn in dataset.Tables[0].Columns)
+                {
+                    if (lineString.Length > 0) { lineString += ","; }
+                    string columnValue = dataRow[dataColumn.ColumnName].ToString();
+                    string dataType = dataRow[dataColumn.ColumnName].GetType().Name;
+                    if (dataType.Equals("String") || dataType.Equals("DateTime"))
+                    {
+                        if (useDoubleQuotesAroundColumnValue) { lineString += $"\"{columnValue}\""; }
+                        else { lineString += $"{columnValue}"; }
+
+                    }
+                    else
+                    {
+                        lineString += $"{columnValue}";
+                    }
+                }
+                lineString += "\r\n";
+                csvString += lineString;
+            }
+
+
+            return csvString;
+        }
+
+        public static string addZeros(string source, int noOfZeros, bool trimToLength)
+        {
+            while (source.Length < noOfZeros) { source = "0" + source; }
+
+            while (trimToLength && source.Length > noOfZeros)
+            {
+                source = source.Substring(1, source.Length - 1);
+            }
+            return source;
+        }
+
+        public static string ConvertStringToLowerPlusFirstCaps(string sourceString)
+        {
+            sourceString = sourceString.ToLower();
+            if (sourceString.Length < 2)
+            {
+                sourceString = sourceString.ToUpper();
+            }
+            else
+            {
+                sourceString = sourceString.Substring(0, 1).ToUpper() + sourceString.ToLower().Substring(1, sourceString.Length - 1);
+            }
+
+            return sourceString;
+        }
+    }
+}
