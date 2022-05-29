2379aadb9a1ee1303e5318a4189a35c20b2c56cf
SumterUtilitiesInc <shtaylor@suimail.com>
2022-05-29 09:31:40 -0400

    Initialize the solution with the empty project architecture

---
+++ SandlotWizards_dotnet_core/src/SandlotWizards/Services/CommandProcessor/CommandConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandlotWizards.CommandConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SandlotWizards_dotnet_core/src/SandlotWizards/Services/CommandProcessor/CommandScheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandlotWizards.CommandScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
